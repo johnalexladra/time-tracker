51ce0239b8715b085eaa121152303e0711a8c4df
John Alex Ladra <johnalexladra@gmail.com>
2023-09-10 16:09:58 +0800

    chore: init route, model and controller

---
+++ server/app/controllers/user.controller.js
@@ -0,0 +1,29 @@
+// Create and Save a new user
+exports.create = (req, res) => {
+
+};
+
+// Retrieve all users from the database.
+exports.findAll = (req, res) => {
+
+};
+
+// Find a single user with an id
+exports.findOne = (req, res) => {
+
+};
+
+// Update a user by the id in the request
+exports.update = (req, res) => {
+
+};
+
+// Delete a user with the specified id in the request
+exports.delete = (req, res) => {
+
+};
+
+// Delete all users from the database.
+exports.deleteAll = (req, res) => {
+
+};
---
+++ server/app/models/user.model.js
@@ -0,0 +1,18 @@
+module.exports = (sequelize, Sequelize) => {
+    const User = sequelize.define("users", {
+        id: {
+            type: Sequelize.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        firstName: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        lastName: {
+            type: Sequelize.STRING
+        }
+    });
+
+    return Log;
+};
